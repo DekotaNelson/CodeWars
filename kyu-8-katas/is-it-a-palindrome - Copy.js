7dadbba849e1ba5f100ab25c108a26e8744f3375
Dekota Nelson <nelson.dekota@gmail.com>
2022-04-16 23:19:09 -0400

    updated file

--- kyu-8-katas/is-it-a-palindrome - Copy.js
+++ kyu-8-katas/is-it-a-palindrome - Copy.js
@@ -1,5 +1,10 @@
 // Write a function that checks if a given string (case insensitive) is a palindrome. (A palindrome is a word, number, phrase, or other sequence of characters which reads the same backward as forward, such as madam or racecar.)
 
-function isPalindrome(x) {
-    return x.toLowerCase() === x.toLowerCase().split('').reverse().join('');
+function removeZeros(x) {
+   while(String(x).endsWith('0')){
+    x = x.slice(0, (x.length - 1));
   }
+  return x;
+}
+
+  console.log(isPalindrome('1110000'))
