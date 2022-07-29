877967ee56d88ab98f17792da0f94aa9cd708591
Dekota Nelson <nelson.dekota@gmail.com>
2022-07-29 16:40:00 -0400

    Added solutions

---
+++ kyu-8-katas/century-from-year.js
@@ -0,0 +1,23 @@
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+// Task
+// Given a year, return the century it is in.
+
+// Examples
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+
+function century(year) {
+    let strYear = year.toString();
+    if(strYear.length < 3){
+      return 1;
+    }
+    else if(strYear.slice(-2) === '00'){
+      return Number(strYear.slice(0, (strYear.length - 2)));
+    }
+    else{
+      return Number(strYear.slice(0, (strYear.length - 2))) + 1;
+    }
+  }
