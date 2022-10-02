fcedec4df07572674464ddcfc605409c2a8e3152
Nelsondl <nelson.dekota@gmail.com>
2022-10-02 10:58:39 -0400

    Added solutions

--- kyu-5-katas/simple-pig-latin.js
+++ kyu-5-katas/simple-pig-latin.js
@@ -3,3 +3,7 @@
 
 // pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
 // pigIt('Hello world !');     // elloHay orldway !
+
+function pigIt(str){
+    return str.split(' ').map(e => e.toLowerCase() != e.toUpperCase() ? `${e.slice(1)}${e.slice(0,1)}ay` : e).join(' ')
+  }
